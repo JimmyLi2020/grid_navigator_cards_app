0f1d916802c5a36670ddd7dd9456c4c98c5ebf52
JimmyLi2020 <lix@avonoldfarms.com>
2020-03-04 11:17:19 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage('Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage(this.title, {Key key});
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: GridView(
+        padding: const EdgeInsets.all(50),
+
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  MyCard(this.text, this.onPressed);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Text(text),
+
+    );
+  }
+}
